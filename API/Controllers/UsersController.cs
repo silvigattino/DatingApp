b2a458e03d57448517db4c651dae61788ed19592
Silvina Gattino <sgattino@ilshealth.net>
2024-03-04 09:29:53 -0500

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,31 +1,33 @@
 
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]     //  /api/users
-public class UsersController : ControllerBase
+namespace API.Controllers
 {
-    private readonly DataContext _context;
-
-    public UsersController(DataContext context)
-   {
-        _context = context;
-   }
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>>GetUsers()
+    [Authorize]
+    public class UsersController : BaseApiController
     {
-        return await _context.Users.ToListAsync();
-    }
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+                _context = context;
+        }
 
-    [HttpGet("{id}")]   //  /api/users/2
-    public async Task<ActionResult<AppUser>>GetUser(int id)
-    {
-        return await _context.Users.FindAsync(id);        
-    }
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>>GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+       
 
+        [HttpGet("{id}")]   //  /api/users/2
+        public async Task<ActionResult<AppUser>>GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);        
+        }
+    }
 }
